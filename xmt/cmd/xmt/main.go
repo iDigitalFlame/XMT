d32c8dad486a3b359de620f806458362c6fb4569
iDigitalFlame <idf@idigitalflame.com>
2019-09-11 13:51:53 -0400

    Cleaned up main funx

--- xmt/cmd/xmt/main.go
+++ xmt/cmd/xmt/main.go
@@ -14,7 +14,6 @@ import (
 	"github.com/iDigitalFlame/xmt/xmt/com/c2/wrapper"
 	"github.com/iDigitalFlame/xmt/xmt/crypto/cbk"
 	"github.com/iDigitalFlame/xmt/xmt/crypto/xor"
-	"github.com/iDigitalFlame/xmt/xmt/device/local"
 	"github.com/iDigitalFlame/xmt/xmt/util"
 )
 
@@ -34,93 +33,77 @@ func testWebC2() {
 		}
 	})
 
-	//xo := xor.Cipher([]byte("derp string"))
+	aesBlock, _ := aes.NewCipher([]byte("123456789012345678901234567890AB"))
+	aesWrapper, _ := wrapper.NewCryptoBlock(aesBlock, []byte("ABCDEF1234567890"))
 
-	//y, _ := wrapper.NewCrypto(xo, xo)
+	cbkWriter := cbk.NewCipher(100)
+	cbkWriter.A = 20
+	cbkWriter.B = 30
+	cbkWriter.C = 40
+	cbkReader := cbk.NewCipher(100)
+	cbkReader.A = 20
+	cbkReader.B = 30
+	cbkReader.C = 40
 
-	z1, err := aes.NewCipher([]byte("123456789012345678901234567890AB"))
-	if err != nil {
-		panic(err)
-	}
-	y, _ := wrapper.NewCryptoBlock(z1, []byte("ABCDEF1234567890"))
+	cbkWrapper, _ := wrapper.NewCrypto(cbkReader, cbkWriter)
 
-	x := cbk.NewCipher(100)
-	x.A = 20
-	x.B = 30
-	x.C = 40
+	xorCipher := xor.Cipher([]byte("this is my xor key"))
+	xorWrapper, _ := wrapper.NewCrypto(xorCipher, xorCipher)
 
-	xr := cbk.NewCipher(100)
-	xr.A = 20
-	xr.B = 30
-	xr.C = 40
-
-	x1, _ := wrapper.NewCrypto(x, xr)
-
-	xx := xor.Cipher([]byte("this is my xor key"))
-	x2, _ := wrapper.NewCrypto(xx, xx)
-
-	p := &c2.Profile{
+	profile := &c2.Profile{
 		Sleep:     time.Duration(5) * time.Second,
-		Transform: transform.DNS,
-		Wrapper:   wrapper.NewMulti(wrapper.Zlib, y, x1, x2),
+		Wrapper:   wrapper.NewMulti(wrapper.Zlib, aesWrapper, cbkWrapper, xorWrapper),
+		Transform: transform.Base64,
 	}
 
-	g := &tcp.WebGenerator{
+	generator := &tcp.WebGenerator{
 		URL:  util.Matcher("/post/%31fd/%12fd/%31fd/edit/%4fl"),
 		Host: util.Matcher("%6fl.myblogsite.com"),
 	}
 
-	s := tcp.NewWeb(p.Sleep)
-	s.Handle("/", http.FileServer(http.Dir("/tmp/")))
+	c2Web := tcp.NewWeb(profile.Sleep)
+	c2Web.Handle("/", http.FileServer(http.Dir("/tmp/")))
 
-	s.Rule(&tcp.WebRule{
-		URL:  g.URL.(util.Matcher).Match(),
-		Host: g.Host.(util.Matcher).Match(),
+	c2Web.Rule(&tcp.WebRule{
+		URL:  generator.URL.(util.Matcher).Match(),
+		Host: generator.Host.(util.Matcher).Match(),
 	})
 
-	//var err error
-	h, err := c2.Controller.Listen("http", "127.0.0.1:8080", s, p)
+	_, err := c2.Controller.Listen("http", "127.0.0.1:8080", c2Web, profile)
 	if err != nil {
 		panic(err)
 	}
 
 	time.Sleep(3 * time.Second)
 
-	c, err := c2.Controller.Connect("http://127.0.0.1:8080", &tcp.WebClient{Generator: g}, p)
+	c2Client, err := c2.Controller.Connect("http://127.0.0.1:8080", &tcp.WebClient{Generator: generator}, profile)
 	if err != nil {
 		panic(err)
 	}
 
 	time.Sleep(5 * time.Second)
 
-	o := &com.Packet{ID: 910}
-	o.Flags |= com.FlagFrag
-	o.Flags.SetFragTotal(2)
-	o.Flags.SetFragGroup(999)
-	o.Flags.SetFragPosition(0)
-	o.WriteString("derp!")
-	fmt.Printf("p1: %s, %s\n", o, o.Flags)
-	c.WritePacket(o)
+	firstPacket := &com.Packet{ID: 910}
+	firstPacket.Flags.SetFragTotal(2)
+	firstPacket.Flags.SetFragGroup(999)
+	firstPacket.Flags.SetFragPosition(0)
+	firstPacket.WriteString("testing string 1!")
+	fmt.Printf("Sent packet 1: %s\n", firstPacket)
+	c2Client.WritePacket(firstPacket)
 
 	time.Sleep(5 * time.Second)
-	q := &com.Packet{ID: 910}
-	q.Flags |= com.FlagFrag
-	q.Flags.SetFragTotal(2)
-	q.Flags.SetFragGroup(999)
-	q.Flags.SetFragPosition(1)
-	q.WriteString("derp!")
-	fmt.Printf("p2: %s, %s\n", q, q.Flags)
-	c.WritePacket(q)
 
-	fmt.Printf("%s\n", h.Session(local.ID()))
+	secondPacket := &com.Packet{ID: 910}
+	secondPacket.Flags.SetFragTotal(2)
+	secondPacket.Flags.SetFragGroup(999)
+	secondPacket.Flags.SetFragPosition(1)
+	secondPacket.WriteString("testing string 2!")
+	fmt.Printf("Sent packet 2: %s\n", secondPacket)
+	c2Client.WritePacket(secondPacket)
 
 	time.Sleep(10 * time.Second)
 
-	c.Shutdown()
+	c2Client.Shutdown()
 
 	time.Sleep(15 * time.Second)
-
-	fmt.Printf("%s\n", h.Session(local.ID()))
-
-	c2.Controller.Wait()
 }
