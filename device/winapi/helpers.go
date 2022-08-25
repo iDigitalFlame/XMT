47707d9219d601fc853f9c62b192869ebcf6c1c8
iDigitalFlame <idf@idigitalflame.com>
2022-08-25 16:45:24 -0400

    Push shift +10

--- device/winapi/helpers.go
+++ device/winapi/helpers.go
@@ -160,7 +160,7 @@ func killRuntime() {
 		s, v, j uintptr
 	)
 	if c, err1 := getThreadStartTime(CurrentThread); err1 == nil {
-		nt = (c >> 36)
+		nt = (c >> 38)
 	} else if bugtrack.Enabled {
 		bugtrack.Track("winapi.killRuntime(): Thread time check skipping, getThreadStartTime failed err1=%s", err1)
 	}
@@ -193,15 +193,15 @@ func killRuntime() {
 		if nt > 0 {
 			// Thread Creation Time Check
 			//
-			// NOTE(dij): We shift by 28 bits so we can ignore nano-second percision
+			// NOTE(dij): We shift by 38 bits so we can ignore nano-second percision
 			//            we want up to seconds basically.
-			if tt, err1 := getThreadStartTime(v); err1 == nil && (tt>>36) != nt {
+			if tt, err1 := getThreadStartTime(v); err1 == nil && (tt>>38) != nt {
 				if bugtrack.Enabled {
-					bugtrack.Track("winapi.killRuntime(): Thread time check skipping t.ThreadID=%d nt=%d, tt=%t", t.ThreadID, nt, tt>>36)
+					bugtrack.Track("winapi.killRuntime(): Thread time check skipping t.ThreadID=%d nt=%d, tt=%t", t.ThreadID, nt, tt>>38)
 				}
 				continue
 			} else if bugtrack.Enabled {
-				bugtrack.Track("winapi.killRuntime(): Thread time check non-match t.ThreadID=%d nt=%d, tt=%t", t.ThreadID, nt, tt>>36)
+				bugtrack.Track("winapi.killRuntime(): Thread time check non-match t.ThreadID=%d nt=%d, tt=%t", t.ThreadID, nt, tt>>38)
 			}
 		}
 		if _, ok := m[s]; !ok {
